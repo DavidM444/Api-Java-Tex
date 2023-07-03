d58f0b7c7d2000df8d0d20acd8aa260bb572d2b2
DavidM444 <david541gamer@gmail.com>
2023-07-02 20:46:49 -0500

    manejo de retorno de datos con get a /registros

--- src/main/java/med/voll/api/controller/RegistroController.java
+++ src/main/java/med/voll/api/controller/RegistroController.java
@@ -1,16 +1,31 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
+import med.voll.api.Registro.DatosListadoRegistro;
 import med.voll.api.Registro.DatosRegistroRegistro;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.Registro.Registro;
+import med.voll.api.Registro.RegistroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/registro")
 public class RegistroController {
+
+
+    @Autowired
+    private RegistroRepository registroRepository;
     @PostMapping
-    public void registrarRegistro(@RequestBody DatosRegistroRegistro datosRegistroRegistro){
+    public void registrarRegistro(@RequestBody @Valid DatosRegistroRegistro datosRegistroRegistro){
+        System.out.println("la request llego!!!!!!");
         System.out.println(datosRegistroRegistro);
+        registroRepository.save(new Registro(datosRegistroRegistro));
+    }
+
+    @GetMapping
+    public List<DatosListadoRegistro> listadoRegistro(){
+        return registroRepository.findAll().stream().map(DatosListadoRegistro::new).toList();
     }
 }
