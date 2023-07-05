f7fe38254052714f3e23ca4e675805581eadbcc1
DavidM444 <david541gamer@gmail.com>
2023-07-05 17:09:27 -0500

    repository de dimensiones

--- src/main/java/med/voll/api/Dimensiones/DatosDimensiones.java
+++ src/main/java/med/voll/api/Dimensiones/DatosDimensiones.java
@@ -3,5 +3,5 @@ package med.voll.api.Dimensiones;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
 
-public record DatosRegistroDimensions(@RequestBody @Valid double altura, double ancho, Integer registro_re_id) {
+public record DatosRegistroDimensiones(@RequestBody @Valid double altura, double ancho, Long registro_re_id) {
 }
---
+++ src/main/java/med/voll/api/Dimensiones/DimensionesRepository.java
@@ -0,0 +1,6 @@
+package med.voll.api.Dimensiones;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DimensionesRepository extends JpaRepository<Dimensiones,Long> {
+}
