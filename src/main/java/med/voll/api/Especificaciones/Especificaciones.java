9a9767f9d5dc6f421e2ab4d0d1c1670fdfe5a3e1
DavidM444 <david541gamer@gmail.com>
2023-07-07 19:48:00 -0500

    especificaciones

---
+++ src/main/java/med/voll/api/Especificaciones/Especificaciones.java
@@ -0,0 +1,12 @@
+package med.voll.api.Especificaciones;
+
+import jakarta.persistence.*;
+
+@Table(name = "especificacion")
+public class Especificaciones {
+    private String es_rollo;
+    private String es_peso;
+    private String es_tipoTela;
+    private Long registro_re_id;
+}
+
