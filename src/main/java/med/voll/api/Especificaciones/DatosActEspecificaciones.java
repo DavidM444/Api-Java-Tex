aba398d93c424808a484aa7ba170f9ccd7ebfc18
DavidM444 <david541gamer@gmail.com>
2023-07-10 18:15:02 -0500

    dto especificaciones

---
+++ src/main/java/med/voll/api/Especificaciones/DatosActEspecificaciones.java
@@ -0,0 +1,4 @@
+package med.voll.api.Especificaciones;
+
+public record DatosActEspecificaciones(String rollo, String peso,String color, String tipoTela) {
+}
---
+++ src/main/java/med/voll/api/Especificaciones/DatosEspecificaciones.java
@@ -0,0 +1,5 @@
+package med.voll.api.Especificaciones;
+
+public record DatosEspecificaciones(String es_rollo,String es_peso,String es_tipoTela,
+                                    String es_color) {
+}
