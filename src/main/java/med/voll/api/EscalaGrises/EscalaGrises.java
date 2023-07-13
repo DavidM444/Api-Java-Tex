6dbf40ee02d59f1e41a765b977af26693f9bbf7e
DavidM444 <david541gamer@gmail.com>
2023-07-12 19:15:45 -0500

    esg

---
+++ src/main/java/med/voll/api/EscalaGrises/EscalaGrises.java
@@ -0,0 +1,26 @@
+package med.voll.api.EscalaGrises;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "escalaGrises")
+public class EscalaGrises {
+    private Integer esg_calificacion;
+    private String esg_valoracion;
+    @Id
+    private Long registro_re_id;
+    public EscalaGrises(DatosEscalaGrises datosEscalaGrises) {
+        this.esg_calificacion = datosEscalaGrises.esg_valoracion();
+    }
+    public void actualizarDatos(DatosActEscg datosActEscg) {
+        this.esg_calificacion = datosActEscg.esg_calificacion();
+    }
+}
