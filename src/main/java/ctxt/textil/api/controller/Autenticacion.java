aff6765779eaa552ceb5b9e6927f1aebf343aa2c
DavidM444 <david541gamer@gmail.com>
2023-07-19 20:40:01 -0500

    autenticacion

---
+++ src/main/java/ctxt/textil/api/controller/Autenticacion.java
@@ -0,0 +1,32 @@
+package ctxt.textil.api.controller;
+
+import ctxt.textil.api.Usuario.DatosAutenticarUsuario;
+import ctxt.textil.api.Usuario.Usuario;
+import ctxt.textil.api.infra.security.DatosJwtToken;
+import ctxt.textil.api.infra.security.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class Autenticacion {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutenticarUsuario datosAutenticarUsuario){
+        System.out.println("llego a autenticar: "+ datosAutenticarUsuario);
+        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticarUsuario.email(),datosAutenticarUsuario.clave());
+        var usuarioAutenticado = authenticationManager.authenticate(token);
+        var jwtToken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
+        return ResponseEntity.ok(new DatosJwtToken(jwtToken));
+
+    }
+}
