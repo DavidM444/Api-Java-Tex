416ce5f089fd0e5d39f641fca9731c4e29b10241
DavidM444 <david541gamer@gmail.com>
2023-07-20 19:25:44 -0500

    respuesta json user no logueado

--- src/main/java/ctxt/textil/api/infra/security/SecurityFilter.java
+++ src/main/java/ctxt/textil/api/infra/security/SecurityFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -33,6 +35,14 @@ public class SecurityFilter extends OncePerRequestFilter {
                 var autenticacion = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(autenticacion);
             }
+        }else {
+            int res =response.getStatus();
+            System.out.println(res);
+            //forbbiden, solicitud correcta con restriccion.
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write("{\"message\": \"Usuario no logueado\" ->\"Credenciales incorrectas\"}");
+            return;
         }
         filterChain.doFilter(request,response);
     }
