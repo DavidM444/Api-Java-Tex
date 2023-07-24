cb092e31481b7b9ab5b467a74ac95c2092eebbe1
DavidM444 <david541gamer@gmail.com>
2023-07-24 06:58:11 -0500

    Path login, controller

--- src/main/java/ctxt/textil/api/controller/Autenticacion.java
+++ src/main/java/ctxt/textil/api/controller/Autenticacion.java
@@ -12,7 +12,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("login")
 public class Autenticacion {
     @Autowired
     private AuthenticationManager authenticationManager;
