5a8fadf439b3f898802ca06424e4d47dbf53c2d4
DavidM444 <david541gamer@gmail.com>
2023-11-11 16:55:21 -0500

    writer deleted

--- src/main/java/ctxt/textil/api/infra/security/SecurityFilter.java
+++ src/main/java/ctxt/textil/api/infra/security/SecurityFilter.java
@@ -5,7 +5,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,32 +26,40 @@ public class SecurityFilter extends OncePerRequestFilter {
     private UserRepository userRespository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenRequest = request.getHeader("authorization");
         System.out.println("token reqeuss: "+ tokenRequest);
         if (tokenRequest !=null){
             var token = tokenRequest.replace("Bearer ", "");
             var nombreUsuario = tokenService.getSubject(token);
             Integer Id = tokenService.getIdClaim(token);
+            System.out.println("token "+token +" id "+ Id);
             request.setAttribute("Idcl",Id);
             if (nombreUsuario!= null){
                 var usuario = userRespository.findByUsEmail(nombreUsuario);
                 var autenticacion = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(autenticacion);
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.getWriter().write("{\"message\": \"Inicio de Exitoso :) \"}");
+
             }
-        }else {
+        }
             String requestURI = request.getRequestURI();
             System.out.println(requestURI);
 
             int res =response.getStatus();
             System.out.println(res);
+
             //forbbiden, solicitud correcta con restriccion.
             //response.setStatus(HttpStatus.FORBIDDEN.value());
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.getWriter().write("{\"message\": \"Inicio de sesión fallido :( \"}");
-            return;
-        }filterChain.doFilter(request,response);
+            System.out.println("escribiendo error");
+            //response.getWriter().write("{\"message\": \"Inicio de sesión fallido :( \"}");
+            System.out.println("error escrito");
+            //return;
+
+
+
+
+        System.out.println("mandando request dofilteer");
+        filterChain.doFilter(request,response);
     }
 }
