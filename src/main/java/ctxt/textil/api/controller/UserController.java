0f71dfc9d8d6293fb83c8b4a29f640293285fdc5
DavidM444 <david541gamer@gmail.com>
2024-01-29 20:23:13 -0500

    Controller for creating administrators

--- src/main/java/ctxt/textil/api/controller/UserController.java
+++ src/main/java/ctxt/textil/api/controller/UserController.java
@@ -1,11 +1,15 @@
 package ctxt.textil.api.controller;
 
 import ctxt.textil.api.Security.Encript.EncriptKey;
+import ctxt.textil.api.UserAdmin.UserAdmin;
+import ctxt.textil.api.UserAdmin.UserAdminRepository;
 import ctxt.textil.api.Usuario.*;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /*
@@ -19,6 +23,11 @@ public class UserController {
     private String apiSecret;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private UserAdminRepository userAdminRepository;
+
+    private final static String RoleUser = "ROLE_USER";
+    private final static String RoleAdmin = "ROLE_USER";
 
     @PostMapping
     public ResponseEntity<String> registroUser(@RequestBody @Valid DatosNewUser datosNewUser)  {
@@ -28,6 +37,19 @@ public class UserController {
         DataUser dataUser = new DataUser(datosNewUser.nombre(), datosNewUser.apellido(), datosNewUser.email(), claveSave);
         System.out.println("datos guarda: "+dataUser);
         Usuario usuario = userRepository.save(new Usuario(dataUser));
+
         return ResponseEntity.ok("Registro exitoso");
+    }
+
+    @Transactional
+    @PostMapping("/admin")
+    //@PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<String> registroUserAdmin(@RequestBody @Valid DatosNewUser dtUser){
+        String clave = EncriptKey.BycriptKeydd(dtUser.clave());
+        DataUser dataUser = new DataUser(dtUser.nombre(),dtUser.apellido(), dtUser.email(),clave);
+
+        UserAdmin usuario = userAdminRepository.save(new UserAdmin(dataUser));
+        System.out.println("dta: "+dtUser+ " role: "+usuario.getAuthorities());
+        return ResponseEntity.ok("Usuario administrador creado");
     }
 }
