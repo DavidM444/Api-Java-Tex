05ab3b0f593dad7a6be91d778b240338042ca12f
DavidM444 <david541gamer@gmail.com>
2024-01-29 20:25:26 -0500

    Repository for administrators users

---
+++ src/main/java/ctxt/textil/api/UserAdmin/UserAdminRepository.java
@@ -0,0 +1,6 @@
+package ctxt.textil.api.UserAdmin;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAdminRepository extends JpaRepository<UserAdmin,Long> {
+}
