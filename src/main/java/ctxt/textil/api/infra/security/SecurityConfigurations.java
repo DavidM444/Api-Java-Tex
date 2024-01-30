4bfbf1d380b562152679c5a83826b177c886de39
DavidM444 <david541gamer@gmail.com>
2024-01-30 16:42:17 -0500

    Refactoring methods deprecated

--- src/main/java/ctxt/textil/api/infra/security/SecurityConfigurations.java
+++ src/main/java/ctxt/textil/api/infra/security/SecurityConfigurations.java
@@ -6,34 +6,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfigurations {
+public class SecurityConfigurations implements WebSecurityCustomizer {
 
     @Autowired
     private  SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws  Exception{
-        return httpSecurity.csrf().disable().cors(Customizer.withDefaults()).sessionManagement().
-                sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-                and().authorizeRequests().requestMatchers(
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable).cors(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
                         new AntPathRequestMatcher("/login",HttpMethod.POST.toString()),
                         new AntPathRequestMatcher("/signup",HttpMethod.POST.toString())
-                )
-                .permitAll()
-                .anyRequest().authenticated().and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                ).permitAll())
+                .authorizeHttpRequests(auth1 -> auth1.requestMatchers(
+                        new AntPathRequestMatcher("/singup/admin",HttpMethod.POST.toString()))
+                        .hasRole("ROLE_ADMIN").anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
@@ -44,5 +54,12 @@ public class SecurityConfigurations {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    public void customize(WebSecurity web) {
+
+
     }
 }
