28aa3048dfc672e015e67ed7bcf7658880b4081c
DavidM444 <david541gamer@gmail.com>
2024-02-02 19:56:36 -0500

    Roles impl

---
+++ src/main/java/ctxt/textil/api/Usuario/Roles/DtoRol.java
@@ -0,0 +1,4 @@
+package ctxt.textil.api.Usuario.Roles;
+
+public record DtoRol(Long idUser, String rolName) {
+}
---
+++ src/main/java/ctxt/textil/api/Usuario/Roles/RolRepository.java
@@ -0,0 +1,6 @@
+package ctxt.textil.api.Usuario.Roles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Integer> {
+}
