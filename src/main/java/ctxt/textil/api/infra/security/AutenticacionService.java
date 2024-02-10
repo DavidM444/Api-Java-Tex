a91d51fcd0088950107fd89d8b40e561e15c1d78
DavidM444 <david541gamer@gmail.com>
2024-02-09 20:06:08 -0500

    Impl for UserDetails

--- src/main/java/ctxt/textil/api/infra/security/AutenticacionService.java
+++ src/main/java/ctxt/textil/api/infra/security/AutenticacionService.java
@@ -1,7 +1,9 @@
 package ctxt.textil.api.infra.security;
 
+import ctxt.textil.api.UserAdmin.UserAdminRepository;
 import ctxt.textil.api.Usuario.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,8 +14,28 @@ public class AutenticacionService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserAdminRepository userRep;
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsEmail(username);
+        System.out.println("autenticando user: "+username);
+        UserDetails user;
+        user = userRepository.findByUsEmail(username);
+        try {
+            if (user==null){
+                System.out.println("entrando en user null "+username);
+                user = userRep.findByAdEmail(username);
+            }
+        }catch (Exception ex){
+            System.out.println("!!!!!!!!Excepcion!!!!!!!!!!!!!!!!!!" + ex.getMessage());
+            throw new RuntimeException(ex);
+
+        }
+        Boolean d = user == null;
+        System.out.println("antes de retornar user "+user.getUsername()+" bool "+d);
+        return user;
     }
+
 }
